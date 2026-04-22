b1fbfdc26b5c1c72d579be094f8de5001322f79a
Mochi <mochi@egghead-2.local>
2026-04-22 15:34:18 +0800

    Update middleware
    Co-Authored-By: Jennifer Bautista <72844242+jlusong15@users.noreply.github.com>

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,16 +1,24 @@
 import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+	'strapi::logger',
+	'strapi::errors',
+	'strapi::security',
+	{
+		name: "strapi::cors",
+		config: {
+			origin: [
+				"http://localhost:3000",
+				"https://nextjs-strapi-sepia.vercel.app",
+			],
+		},
+	},
+	'strapi::poweredBy',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
 ];
 
 export default config;
