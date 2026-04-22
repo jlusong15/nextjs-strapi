8b773dd83bb57a27cd92cae56b65dea87aaf7316
Mochi <mochi@egghead-2.local>
2026-04-22 10:44:06 +0800

    Update book reviews page and add thumbnail
    Co-Authored-By: Jennifer Bautista <72844242+jlusong15@users.noreply.github.com>

--- frontend/app/book-reviews/page.tsx
+++ frontend/app/book-reviews/page.tsx
@@ -3,24 +3,31 @@ import SubPageLayout from "../components/layout/Subpages"
 import StarRating from "../components/shared/Rating"
 import { fetchBookReviews } from "../services/book-reviews.service"
 import StrapiRichTextBlocks from "../components/shared/StrapiRichTextBlock"
+import StrapiImage from "../components/shared/StrapiImage"
 
 export default async function BookReview() {
 	const bookReviews = await fetchBookReviews()
 
 	return (
-		<div className="max-w-2xl w-full">
+		<div className="max-w-4xl w-full">
 			<SubPageLayout title="Book Reviews">
-				<small className="text-gray-500!">Details are retrieved from Strapi through a Next.js API proxy.</small>
+				<div className="border-b border-b-gray-100 pb-2 mb-5">
+					<small className="text-gray-500!">Details are retrieved from Strapi through a Next.js API proxy.</small>
+				</div>
 				{bookReviews?.map((review, i) => (
 					<div key={i} className="flex flex-col py-5 border-b border-gray-200 my-2.5 gap-3">
 						<div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
 							<h3>{review.title}</h3> <StarRating value={review.rating} readonly />
 						</div>
-						<div className="line-clamp-4">
-							<p>
+						<div className="flex flex-row gap-2 items-start">
+							<div className="mr-2 mb-2">
+								<StrapiImage image={review.image} size="thumbnail" className="max-w-23" />
+							</div>
+							<div className="line-clamp-4">
 								<StrapiRichTextBlocks content={review.content} isPlain={true} />
-							</p>
+							</div>
 						</div>
+
 						<div className="text-right">
 							<Link
 								href={"/book-reviews/" + review?.documentId}
