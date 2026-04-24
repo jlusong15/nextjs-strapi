7b96baddb92bbd04cc7ec89c3038a6ae2aaf6c2d
Mochi <mochi@egghead-2.local>
2026-04-24 14:25:36 +0800

    update next config
    Co-Authored-By: Jennifer Bautista <72844242+jlusong15@users.noreply.github.com>

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-    domains: ["localhost", "nextjs-strapi-production.up.railway.app"],
-  },
+		domains: ["localhost", "nextjs-strapi-production.up.railway.app", "nextjs-strapi-sepia.vercel.app"],
+	},
 };
 
 export default nextConfig;
