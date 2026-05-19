6b27d11b9c91baabc0c7a0d8097a38aab814e194
Mochi <jennalilubautista@gmail.com>
2026-05-19 15:38:39 +0800

    Update provider

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import TanstackProvider from "@/components/providers/tanstack-provider"
+import TanstackProvider from "@/components/providers/tanstack.provider"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return <TanstackProvider>{children}</TanstackProvider>
